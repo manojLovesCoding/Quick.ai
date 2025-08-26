bf29218515f2cb977189f79c7fddbff4ff1959e6
manojLovesCoding <mkum9417@gmail.com>
2025-08-26 15:34:07 +0530

    feat: integrate Clerk authentication middleware
    Added Clerk middleware for authentication and user management, Applied clerkMiddleware globally to handle auth context, Secured routes using requireAuth to ensure only authenticated users can access them, Kept / route public as a health check endpoint

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => res.send("Hello from server"));
-
+app.use(requireAuth());
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
